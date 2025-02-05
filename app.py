17ee5eb16dd55f1c3124baf6489f56758adf1ba9
parth-18-bansal <parthbansalcoding@gmail.com>
2025-02-06 01:25:31 +0530

    add the configuration for the loggin using elk stack

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 # app.py
 from flask import Flask
 from prometheus_client import start_http_server, Counter
+import logging
+from logstash import LogstashHandler
 
 app = Flask(__name__)
 
 # Create a counter metric to track requests
 REQUESTS = Counter('http_requests_total', 'Total number of HTTP requests')
 
+# Set up logging to Logstash
+logger = logging.getLogger('python-app')
+logger.setLevel(logging.INFO)
+logstash_handler = LogstashHandler('logstash', 5044)  # Default Logstash port
+logger.addHandler(logstash_handler)
+
 @app.route('/')
 def hello():
     REQUESTS.inc()  # Increment the request counter
+    logger.info('Request received to / endpoint')
     return "Hello, World!"
 
 if __name__ == '__main__':
